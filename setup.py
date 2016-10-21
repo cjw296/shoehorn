204f0ac465388800066f12579de6c830bbfc27a7
Chris Withers <chris@simplistix.co.uk>
2016-10-21 15:29:17 +0100

    prepare for 0.1.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ base_dir = os.path.dirname(__file__)
 
 setup(
     name='shoehorn',
-    version='0.1.5',
+    version='0.1.6',
     author='Chris Withers',
     author_email='chris@withers.org',
     license='MIT',
