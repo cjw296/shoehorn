4c642a19ff8d9565f3d5f5c45659ad7b19f45ef6
wim glenn <wim.glenn@gmail.com>
2017-07-05 14:19:00 -0500

    properly exclude "tests" subpackage

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    packages=find_packages(exclude='tests'),
+    packages=find_packages(exclude=['tests']),
     zip_safe=False,
     include_package_data=True,
     extras_require=dict(
