0205ee76d7f65928663acdef744b355c06926026
Chris Withers <chris@simplistix.co.uk>
2018-05-31 18:48:55 +0100

    Simple layer for fanning out handlers.

--- shoehorn/targets.py
+++ shoehorn/targets.py
@@ -11,3 +11,20 @@ class Stack(object):
             event = target(event)
             if event is None:
                 break
+
+
+class Layer(object):
+
+    def __init__(self, *handlers, propagate=False):
+        self.targets = []
+        self.targets.extend(handlers)
+        self.propagate = propagate
+
+    def add(self, handler):
+        self.targets.append(handler)
+
+    def __call__(self, event):
+        for target in self.targets:
+            target(event)
+        if self.propagate:
+            return event
--- tests/test_targets.py
+++ tests/test_targets.py
@@ -1,6 +1,6 @@
 from testfixtures import compare
 
-from shoehorn.targets import Stack
+from shoehorn.targets import Stack, Layer
 from shoehorn.testing import TestTarget
 
 
@@ -42,3 +42,54 @@ class TestStack(object):
         s.push(handle, t)
         s('event')
         compare(t.events, expected=['something else'])
+
+
+class TestLayer(object):
+
+    def test_empty(self):
+        Layer()('event')
+
+    def test_one(self):
+        t = TestTarget()
+        l = Layer()
+        l.add(t)
+        l('event')
+        compare(t.events, expected=['event'])
+
+    def test_two(self):
+        t1 = TestTarget()
+        t2 = TestTarget()
+        l = Layer()
+        l.add(t1)
+        l.add(t2)
+        l('event')
+        compare(t1.events, expected=['event'])
+        compare(t2.events, expected=['event'])
+
+    def test_replace_event(self):
+        def handle(event):
+            return 'something else'
+        t = TestTarget()
+        l = Layer()
+        l.add(handle)
+        l.add(t)
+        l('event')
+        compare(t.events, expected=['event'])
+
+    def test_constructor(self):
+        t1 = TestTarget()
+        t2 = TestTarget()
+        l = Layer(t1, t2)
+        l('event')
+        compare(t1.events, expected=['event'])
+        compare(t2.events, expected=['event'])
+
+    def test_propagate(self):
+        l = Layer(propagate=True)
+        result = l('event')
+        assert result == 'event'
+
+    def test_no_propagate(self):
+        l = Layer()
+        result = l('event')
+        assert result is None
