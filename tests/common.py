4609ae67dd40a0cd3160d76bc24c3f4a10fedf9d
Chris Withers <chris@simplistix.co.uk>
2018-07-05 08:05:55 +0100

    add stream redirection support

--- tests/common.py
+++ tests/common.py
@@ -1,11 +1,11 @@
 import sys
-from subprocess import check_output
+from subprocess import check_call
 from textwrap import dedent
 
 
-def run_in_ascii(dir, code):
+def run_in_ascii(dir, code, **kw):
     # heavy, but the only way to test that we're handling encoding
     # errors properly
     path = dir.write('test.py', encoding='utf-8', data=dedent(code))
-    return check_output([sys.executable, path, dir.getpath('test.log')],
-                        env={'LC_ALL': 'C'})
+    return check_call([sys.executable, path, dir.getpath('test.log')],
+                      env={'LC_ALL': 'C'}, **kw)
